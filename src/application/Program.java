636566944308e3620e9fe3a21bc0076fe28a39d9
Carlos Cogo <carloscogo84@gmail.com>
2023-05-02 08:38:16 +0100

    Handling exceptions

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,27 +2,36 @@ package application;
 
 import bordgame.Board;
 import bordgame.Position;
+import chess.ChessException;
 import chess.ChessMacth;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ChessMacth chessMacth = new ChessMacth();
+
         while (true) {
-            UI.printBoard(chessMacth.getPices());
-            System.out.println();
-            System.out.print("Source: ");
-            ChessPosition source = UI.readChesPosition(sc);
+            try {
+                UI.clearScreen();
+                UI.printBoard(chessMacth.getPices());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChesPosition(sc);
 
-            System.out.println();
-            System.out.print("Target: ");
-            ChessPosition target = UI.readChesPosition(sc);
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChesPosition(sc);
 
-            ChessPiece capturedPiece = chessMacth.performChessMove(source, target);
+                ChessPiece capturedPiece = chessMacth.performChessMove(source, target);
+            }
+            catch (ChessException | InputMismatchException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
         }
 
     }
